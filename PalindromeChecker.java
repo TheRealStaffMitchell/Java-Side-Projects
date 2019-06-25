999b5a05e13ea29c8891d7499a93a5abee86dcc8
TheRealStaffMitchell <52115537+TheRealStaffMitchell@users.noreply.github.com>
2019-06-25 02:34:17 -0400

    Update PalindromeChecker.java
    This is a simple Java program that checks to see if a number entered is a palindrome. Whatever is input is stored as the string object "userNum". A StringBuilder object is created and initialized to the contents of the string userNum. The StringBuilder method reverse() is used to reverse the characters of the StringBuilder object before the toString() method is used to return a string with the data. Finally, an if statement is used to determine whether the reverse string is equal to the one input by the user. If so, the output specifies. If not, the argument for the else statement is satisfied and the string is not a palindrome. The output also specifies this to the user. Technically, this works for more than just numbers. Since a string is being entered, any sequence of characters such as a word or a name will still work.

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PalindromeChecker{
 
       public static void main (String[] args){
-            //Initialize variables
+            //Initialize strings
             String userNum;
             String userNumReversed;
             
@@ -11,8 +11,12 @@ public class PalindromeChecker{
             Scanner in = new Scanner(System.in);
             System.out.println ("Please enter a number: ");
             userNum = in.nextLine();
+            
+            //Get the reversed string
             userNumReversed = new StringBuilder(userNum).reverse().toString();
             
+            
+            //Determine if the string is a palindrome
             if (userNum.equals(userNumReversed)){
                   System.out.println (userNum + " is a palindrome.");
             }
